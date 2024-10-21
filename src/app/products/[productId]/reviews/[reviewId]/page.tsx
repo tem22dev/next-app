b17d75d57722e886877302cb242d1108976985d5
Trung Em ^_^ <tem2k2.dev@gmail.com>
2024-10-21 14:34:23 +0700

    Not found

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,5 +1,15 @@
-function ReviewProductDetail({ params }: { params: { productId: string, reviewId: string } }) {
-    return <h1>Review {params.reviewId} for product {params.productId}</h1>;
+import { notFound } from 'next/navigation';
+
+function ReviewProductDetail({ params }: { params: { productId: string; reviewId: string } }) {
+    if (parseInt(params.reviewId) > 1000) {
+        notFound();
+    }
+
+    return (
+        <h1>
+            Review {params.reviewId} for product {params.productId}
+        </h1>
+    );
 }
 
 export default ReviewProductDetail;
